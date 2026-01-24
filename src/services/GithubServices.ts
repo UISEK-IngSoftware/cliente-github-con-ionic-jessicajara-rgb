f600ec7c9a5e2a3fe1b65670c224a1c2604b44c6
jessicajara <jessica.jara@uisek.edu.ec>
2026-01-24 12:28:11 -0500

    resolucion laboratorio 9

--- src/services/GithubServices.ts
+++ src/services/GithubServices.ts
@@ -2,7 +2,16 @@ import axios from "axios";
 import { RepositoryItem } from "../interfaces/RepositoryItems";
 
 const GITHUB_API_URL = "https://api.github.com";
+<<<<<<< Updated upstream
 const GITHUB_API_TOKEN = "ghp_XXXXXXXXXXXXXXXXXXXXXXXXXX"; //Reemplazar por token valida
+=======
+const GITHUB_API_TOKEN = "XXXXXXXXXXXXXXXXXXXXXXXXXXX"; //Reemplazar por token valida
+
+/**
+ * 
+ * @returns Lista de repositorios del usuario autenticado
+ */
+>>>>>>> Stashed changes
 
 export const fetchRepositories = async (): Promise<RepositoryItem[]> => {
     try {
