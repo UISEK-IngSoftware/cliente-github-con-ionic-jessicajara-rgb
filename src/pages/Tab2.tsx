1df1f6f394eae3f12378f396183e3cfb06ba6d12
jessicajara <jessica.jara@uisek.edu.ec>
2026-01-24 12:10:44 -0500

    resolucion de laboratorio 9

--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -11,8 +11,45 @@ import {
 import './Tab2.css';
 import React from 'react';
 import { IonInput } from '@ionic/react';
+import { useHistory } from 'react-router';
+import { RepositoryItem } from '../interfaces/RepositoryItems';
+import { h } from 'ionicons/dist/types/stencil-public-runtime';
+import { createRepository } from '../services/GithubServices';
 
 const Tab2: React.FC = () => {
+  const history = useHistory();
+
+  const repoFormData: RepositoryItem = {
+    name: '',
+    description: '',
+    imageUrl: null,
+    owner: null,
+    language: null,
+  }
+
+  const setRepoName = (value: string) => {
+    repoFormData.name = value;
+  }
+
+  const setDescription = (value: string) => {
+    repoFormData.description = value;
+  }
+
+  const saveRepo = () => {
+    console.log('Guardando repositorio:', repoFormData);
+    if (repoFormData.name.trim() === '') {
+      alert('El nombre del repositorio es obligatorio.');
+      return;
+    }
+
+    createRepository(repoFormData).then(() => {
+      history.push('/tab1');
+    }).catch((error) => {
+      console.error('Error al crear el repositorio:', error);
+      alert('Hubo un error al crear el repositorio. Por favor, inténtalo de nuevo.');
+    });
+  };
+
   return (
     <IonPage>
       <IonHeader>
@@ -29,22 +66,28 @@ const Tab2: React.FC = () => {
 
         <div className="form-container">
           <IonInput
+            className="form-field"
             label="Nombre del Repositorio"
             labelPlacement="floating"
             fill="outline"
             placeholder="repositorio-de-ejemplo"
-            className="form-field"
+            value={repoFormData.name}
+            onIonChange={e => setRepoName(e.detail.value!)}
           ></IonInput>
 
           <IonTextarea
+            className="form-field"
             label="Descripcion del repositorio"
             labelPlacement="floating"
             fill="outline"
             placeholder="este es un repositorio de ejemplo para demostrar el formulario"
-            className="form-field"
-            rows={6}></IonTextarea>
+            value={repoFormData.description}
+            onIonChange={e => setDescription(e.detail.value!)}
+            rows={6}
+            autoGrow
+          ></IonTextarea>
 
-          <IonButton expand="block" className="form-field">Guardar</IonButton>
+          <IonButton expand="block" className="form-field" onClick={saveRepo}>Guardar</IonButton>
 
         </div>
       </IonContent>
--- src/services/GithubServices.ts
+++ src/services/GithubServices.ts
@@ -1,11 +1,18 @@
 import axios from "axios";
 import { RepositoryItem } from "../interfaces/RepositoryItems";
+import { UserInfo } from "../interfaces/UserInfo";
 
 const GITHUB_API_URL = "https://api.github.com";
-const GITHUB_API_TOKEN = "ghp_XXXXXXXXXXXXXXXXXXXXXXXXXX"; //Reemplazar por token valida
+const GITHUB_API_TOKEN = "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"; //Reemplazar por token valida
+
+/**
+ * 
+ * @returns Lista de repositorios del usuario autenticado
+ */
 
 export const fetchRepositories = async (): Promise<RepositoryItem[]> => {
     try {
+        //console.log("TOKEN:", import.meta.env.VITE_GITHUB_API_TOKEN);
         const response = await axios.get(`${GITHUB_API_URL}/user/repos`, {
             headers: {
                 Authorization: `Bearer ${GITHUB_API_TOKEN}`,
@@ -14,9 +21,12 @@ export const fetchRepositories = async (): Promise<RepositoryItem[]> => {
                 per_page: 100,
                 sort: "created",
                 direction: "desc",
-            }
+            },
         });
 
+        console.log("STATUS:", response.status);
+        console.log("DATA:", response.data);
+        console.log("ES ARRAY:", Array.isArray(response.data));
 
         const reposData: RepositoryItem[] = response.data.map((repo: any) => ({
             name: repo.name,
@@ -32,3 +42,36 @@ export const fetchRepositories = async (): Promise<RepositoryItem[]> => {
         return [];
     }
 }
+
+export const createRepository = async (repo: RepositoryItem): Promise<void> => {
+    try {
+        const response = await axios.post(`${GITHUB_API_URL}/user/repos`, repo, {
+            headers: {
+                Authorization: `Bearer ${GITHUB_API_TOKEN}`,
+            },
+        });
+        console.log("Repository created:", response.data);
+    } catch (error) {
+        console.error("Error creating repository:", error);
+    }
+};
+
+/**
+ * 
+ * @returns Informacion del usuario autenticado
+ */
+export const getUserInfo = async (): Promise<UserInfo | null> => {
+    try {
+        const response = await axios.get(`${GITHUB_API_URL}/user`, {
+            headers: {
+                Authorization: `Bearer ${GITHUB_API_TOKEN}`,
+            },
+        })
+        return response.data;
+    } catch (error) {
+        console.error("Error fetching user info:", error);
+        return null;
+    }
+}
+
+
